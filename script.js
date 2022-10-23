f01b942fd25ea1ddc7966a5d8d3ec7e5a9e3bd0e
JQCVSC <65506340+JQCVSC@users.noreply.github.com>
2022-10-23 16:40:05 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -12,11 +12,11 @@ var counter = 0;
 function addToBuyList() {
   counter += 1;
   //counter = counter + 1;
-  if(counter === 1){
-                                //"1 Pet" or below
-    buyCountText.textContent = counter + " Packs"
+  if (counter === 1) {
+    //"1 Pack" or below
+    buyCountText.textContent = counter + " Packs";
   } else {
-    buyCountText.textContent = counter + " Packs"
+    buyCountText.textContent = counter + " Packs";
   }
 }
 
@@ -25,9 +25,33 @@ rnbBtn.addEventListener("click", addToBuyList);
 afrolatinBtn.addEventListener("click", addToBuyList);
 trapBtn.addEventListener("click", addToBuyList);
 
-function emailSubmitHandler(){
+var removeCountText = document.querySelector("#buy-count");
+var rapBtn2 = document.querySelector("#rap-btn2");
+var rnbBtn2 = document.querySelector("#rnb-btn2");
+var afrolatinBtn2 = document.querySelector("#afrolatin-btn2");
+var trapBtn2 = document.querySelector("#trap-btn2");
+
+
+function removeFromBuyList() {
+  counter -= 1;
+  //counter = counter - 1;
+  if (counter === 1) {
+    //"1 Pack" or below
+    removeCountText.textContent = counter + " Packs";
+  } else {
+    removeCountText.textContent = counter + " Packs";
+  }
+}
+
+rapBtn2.addEventListener("click", removeFromBuyList);
+rnbBtn2.addEventListener("click", removeFromBuyList);
+afrolatinBtn2.addEventListener("click", removeFromBuyList);
+trapBtn2.addEventListener("click", removeFromBuyList);
+
+function emailSubmitHandler() {
   var confirmationMsg = document.createElement("p");
-  confirmationMsg.textContent = "Thanks for subscribing! A confirmation email was sent to " + input.value;
+  confirmationMsg.textContent =
+    "Thanks for subscribing! A confirmation email was sent to " + input.value;
   signUpForm.remove();
   footer.appendChild(confirmationMsg);
 }
